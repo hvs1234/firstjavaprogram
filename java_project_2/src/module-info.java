d83477cc2a5f649f9a30e88bc6cdf9888a2fbf42
HARSH SHARMA <HARSH SHARMA@DESKTOP-AKGQNEH>
2022-03-31 10:20:01 +0530

    commit

---
+++ java_project_2/src/module-info.java
@@ -0,0 +1,2 @@
+module java_project_2 {
+}
---
+++ java_project_2/src/unit01/p3_task01_variablesAndDatatypesInJava.java
@@ -0,0 +1,22 @@
+package unit01;
+
+public class p3_task01_variablesAndDatatypesInJava {
+	static int q = 11;
+	int p = 10;
+	static void typeconversioncasting() {
+		int a1 = 4;
+		long b1 = 7808097L;
+		double a2 = 6.0;
+		a2 = (float)(a1);
+		System.out.println(a2);
+		System.out.println((float)(b1));
+	}
+	public static void main(String[] args) {
+	      int a,b,c;
+		  float pi;  double d;  char e;
+		  int g = 5;
+		  System.out.println("Method is ...");
+		  System.out.println(g);
+		  typeconversioncasting();
+	}
+}
